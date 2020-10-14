dd932308da1725f93a904b4e26136f6357ad93e9
john12458 <john210628@gmail.com>
2020-10-14 21:10:00 +0800

    init

---
+++ precache-manifest.e9ba118e611b989bb98b947fef7da782.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8504fb89cdb7a2af5e744ef777d9a5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26f0c1ea772cb1ec634c",
+    "url": "/static/css/main.30ee69e5.chunk.css"
+  },
+  {
+    "revision": "81860a0918fb4394c169",
+    "url": "/static/js/2.ae278f34.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ae278f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f0c1ea772cb1ec634c",
+    "url": "/static/js/main.c9d8ac23.chunk.js"
+  },
+  {
+    "revision": "33742763b190b13623b4",
+    "url": "/static/js/runtime-main.1bdc9327.js"
+  },
+  {
+    "revision": "2109cf925266c27a2d2c13125ff0e726",
+    "url": "/static/media/enemyImage.2109cf92.png"
+  },
+  {
+    "revision": "903a2cc3c512eb7c8a3a56e42219d597",
+    "url": "/static/media/enemyJump.903a2cc3.png"
+  },
+  {
+    "revision": "19314b5f02cfc1e935bb5f39561f626a",
+    "url": "/static/media/error.19314b5f.svg"
+  },
+  {
+    "revision": "9f70da5170c8b98b34e60fc4e843725b",
+    "url": "/static/media/gameBgImage.9f70da51.png"
+  },
+  {
+    "revision": "0c0d0a7bc23460d052f6b87639c86d46",
+    "url": "/static/media/github-logo.0c0d0a7b.svg"
+  },
+  {
+    "revision": "956fff9922958c8df66ba0f15d69a023",
+    "url": "/static/media/myAvatar.956fff99.svg"
+  },
+  {
+    "revision": "6b3d4f65f80ea9cf70b05963f49d59e3",
+    "url": "/static/media/photography.6b3d4f65.svg"
+  },
+  {
+    "revision": "2222b705d5584906fc70dabff7e51f0b",
+    "url": "/static/media/pika.2222b705.svg"
+  },
+  {
+    "revision": "566b774d6cc4108acc54899ef6c2affb",
+    "url": "/static/media/return.566b774d.svg"
+  },
+  {
+    "revision": "5fec04de71ed12aa14bce30d9eba8dda",
+    "url": "/static/media/user.5fec04de.svg"
+  },
+  {
+    "revision": "a6aa8462717212acf3ba3afd30e3e10a",
+    "url": "/static/media/volleyball.a6aa8462.png"
+  }
+]);
